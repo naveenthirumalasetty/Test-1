79c302863ba436de62a87bc8f5265e3364495113
naveenthirumalasetty <97092662+naveenthirumalasetty@users.noreply.github.com>
2022-01-07 13:00:22 +0530

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,8 @@
+def function1():
+    print("Package-->module2-->function1")
+def function2():
+    print("Package-->module2-->function3")
+def function3():
+    print("Package-->module3-->function3")
+def function4():
+    print("Package-->module4-->function4")
